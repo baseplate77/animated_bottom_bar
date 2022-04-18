cef3afa119533c125f254b3b6ab87f29f2cedfaa
baseplate77 <baseplate8087@gmail.com>
2022-04-18 10:40:34 +0530

    refrator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animated_bottom_bar/temp_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
-      // home: const TempScreen(),
+      // home: const DemoScreen(),
     );
   }
 }
--- lib/screen/demo_screen.dart
+++ lib/screen/demo_screen.dart
@@ -1,17 +1,17 @@
-﻿import 'package:animated_bottom_bar/animated_bottom_bar.dart';
+﻿import 'widget/animated_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-class TempScreen extends StatelessWidget {
-  const TempScreen({Key? key}) : super(key: key);
+class DemoScreen extends StatelessWidget {
+  const DemoScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.white,
       bottomNavigationBar: SizedBox(height: 90, child: AnimatedBottomAppBar()),
       body: Center(
         child: Text(
-          "Temp Screen",
+          "Demo Screen",
         ),
       ),
     );
